2ea54ef9b58448a2459541a772b4b49180349ba8
Eric Sjöberg <eric@laurila.se>
2021-01-12 15:59:43 +0100

    Small clean up on background config

--- Assets/Scripts/MenuBackgroundConfig.cs
+++ Assets/Scripts/MenuBackgroundConfig.cs
@@ -2,8 +2,7 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-[Serializable]
-[CreateAssetMenu]
+[Serializable][CreateAssetMenu]
 public class MenuBackgroundConfig : MenuElementConfig {
     
     public Vector2 dimensions = new Vector2(400, 400);
@@ -12,6 +11,5 @@ public class MenuBackgroundConfig : MenuElementConfig {
     public Material backgroundMaterial;
     public bool raycastTarget;
     public Vector4 raycastPadding;
-
     public bool maskable;
 }
