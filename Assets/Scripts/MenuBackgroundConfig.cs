0437c5501a01411aa3cee9ab9a9f86cf7daf51af
Eric Sjöberg <eric@laurila.se>
2021-01-12 15:54:34 +0100

    Adds menu background config

---
+++ Assets/Scripts/MenuBackgroundConfig.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+[Serializable]
+[CreateAssetMenu]
+public class MenuBackgroundConfig : MenuElementConfig {
+    
+    public Vector2 dimensions = new Vector2(400, 400);
+    public Sprite backgroundImage;
+    public Color backgroundColor = new Color(0, 0, 0, 1);
+    public Material backgroundMaterial;
+    public bool raycastTarget;
+    public Vector4 raycastPadding;
+
+    public bool maskable;
+}
