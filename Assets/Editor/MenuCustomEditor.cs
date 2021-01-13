8532dec1e5b10914aff9cd9fd4b7206ccf3430da
Jackoberto <jackoberto01@gmail.com>
2021-01-13 12:52:28 +0100

    And if statements to reduce null references

--- Assets/Editor/MenuCustomEditor.cs
+++ Assets/Editor/MenuCustomEditor.cs
@@ -12,43 +12,48 @@ public class MenuCustomEditor : Editor
     {
         var menu = (Menu) target;
         DrawDefaultInspector();
-        GUILayout.BeginHorizontal();
-        GUILayout.Label("Choose Element:");
-        GUILayout.FlexibleSpace();
-        var height = GUILayout.Height(20);
-        var width = GUILayout.Width(Screen.width * 0.5f);
-        menu.elementToCreate = Mathf.Clamp(Mathf.RoundToInt(GUILayout.HorizontalSlider(menu.elementToCreate, 0, 
-            menu.menuElementConfigs.Length - 1, new GUILayoutOption[]{height, width})),
-            0, menu.menuElementConfigs.Length);
-        GUILayout.FlexibleSpace();
-        width = GUILayout.Width(Screen.width * 0.15f);
-        GUILayout.Label("Element #" + menu.elementToCreate, width);
-        GUILayout.EndHorizontal();
-        var args = menu.menuElementConfigs[menu.elementToCreate].Arguments.ToList();
-        args.Add("Default");
-        args.Reverse();
-        var toolTips = menu.menuElementConfigs[menu.elementToCreate].Descriptions.ToList();
-        toolTips.Add("Creates The Default Menu Element");
-        toolTips.Reverse();
-        if (menu.menuElementConfigs[menu.elementToCreate].canCombineArgs)
+
+        if (menu.menuElementConfigs.Length > 1)
         {
-           if (_optionsTicked.Length != args.Count) 
-               _optionsTicked = new bool[args.Count];
-           for (var i = 1; i < args.Count; i++)
-           {
-               _optionsTicked[i] = GUILayout.Toggle(_optionsTicked[i], new GUIContent(args[i], toolTips[i]));
-           } 
+            GUILayout.BeginHorizontal();
+            GUILayout.Label("Choose Element:");
+            GUILayout.FlexibleSpace();
+            var height = GUILayout.Height(20);
+            var width = GUILayout.Width(Screen.width * 0.5f);
+            menu.elementToCreate = Mathf.Clamp(Mathf.RoundToInt(GUILayout.HorizontalSlider(menu.elementToCreate, 0,
+                    menu.menuElementConfigs.Length - 1, new GUILayoutOption[] {height, width})),
+                0, menu.menuElementConfigs.Length);
+            GUILayout.FlexibleSpace();
+            width = GUILayout.Width(Screen.width * 0.15f);
+            GUILayout.Label("Element #" + menu.elementToCreate, width);
+            GUILayout.EndHorizontal();
         }
-        else
-        {
-            _index = EditorGUILayout.Popup(new GUIContent(args[_index], toolTips[_index]), _index, args.ToArray());
-            _index = Mathf.Clamp(_index, 0, args.Count - 1); 
-        }
-        
-        
+        else menu.elementToCreate = 0;
 
         if (menu.menuElementConfigs.Length > 0 && menu.menuElementConfigs[menu.elementToCreate] != null)
         {
+            var args = menu.menuElementConfigs[menu.elementToCreate].Arguments.ToList();
+            args.Add("Default");
+            args.Reverse();
+            var toolTips = menu.menuElementConfigs[menu.elementToCreate].Descriptions.ToList();
+            toolTips.Add("Creates The Default Menu Element");
+            toolTips.Reverse();
+            if (menu.menuElementConfigs[menu.elementToCreate].canCombineArgs)
+            {
+                if (_optionsTicked.Length != args.Count)
+                    _optionsTicked = new bool[args.Count];
+                for (var i = 1; i < args.Count; i++)
+                {
+                    _optionsTicked[i] = GUILayout.Toggle(_optionsTicked[i], new GUIContent(args[i], toolTips[i]));
+                }
+            }
+            else
+            {
+                _index = EditorGUILayout.Popup(
+                    new GUIContent(args[_index], toolTips[_index]), _index, args.ToArray());
+                _index = Mathf.Clamp(_index, 0, args.Count - 1);
+            }
+
             var subString = $"{menu.menuElementConfigs[menu.elementToCreate].name}";
             var buttonText = $"Add {subString}";
             if (GUILayout.Button(buttonText))
@@ -57,11 +62,12 @@ public class MenuCustomEditor : Editor
                 {
                     menu.menuElementConfigs[menu.elementToCreate].Create(menu.label, menu, ResolveTickedArgs(args));
                 }
-                else 
+                else
                     menu.menuElementConfigs[menu.elementToCreate].Create(menu.label, menu,
-                    _index != 0 ? new [] {args[_index]} : new []{""});
+                        _index != 0 ? new[] {args[_index]} : new[] {""});
             }
         }
+        
         if (GUILayout.Button("Remove Button"))
         {
             menu.RemoveButton();
