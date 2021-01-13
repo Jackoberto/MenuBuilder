20088c2d4e324bed215472b9e273ecd2a3687d0d
Eric Sjöberg <eric@laurila.se>
2021-01-13 10:18:29 +0100

    adds create function background config

--- Assets/Scripts/MenuBackgroundConfig.cs
+++ Assets/Scripts/MenuBackgroundConfig.cs
@@ -3,7 +3,8 @@ using UnityEngine;
 using UnityEngine.UI;
 
 [Serializable][CreateAssetMenu]
-public class MenuBackgroundConfig : MenuElementConfig {
+public class MenuBackgroundConfig : MenuElementConfig 
+{
     
     public Vector2 dimensions = new Vector2(400, 400);
     public Sprite backgroundImage;
@@ -12,4 +13,20 @@ public class MenuBackgroundConfig : MenuElementConfig {
     public bool raycastTarget;
     public Vector4 raycastPadding;
     public bool maskable;
+
+    public override void Create(string name, Menu menu, string[] args)
+    {
+        GameObject image = new GameObject();
+        image.transform.parent = menu.transform;
+        image.name = menu.label;
+        image.AddComponent<RectTransform>().sizeDelta = dimensions;
+        var imageComponent = image.AddComponent<Image>();
+        imageComponent.color = backgroundColor;
+        imageComponent.sprite = backgroundImage;
+        imageComponent.material = backgroundMaterial;
+        imageComponent.raycastTarget = raycastTarget;
+        imageComponent.raycastPadding = raycastPadding;
+        imageComponent.maskable = maskable;
+        base.Create(name, menu, args);
+    }
 }
