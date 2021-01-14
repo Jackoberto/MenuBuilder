4ff368049195d09e61e9bbf25768951db9f63711
Jackoberto <jackoberto01@gmail.com>
2021-01-14 14:32:28 +0100

    Adds MenuBuilder To GameObject Create Context Menu

---
+++ Assets/Scripts/MenuBuild.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuBuild : Editor
+{
+    [MenuItem("GameObject/MenuBuilder", false, 0)]
+    private static void CreateMenu()
+    {
+        var canvas = FindObjectOfType<Canvas>();
+        if (canvas == null)
+        {
+            var myGO = new GameObject("Canvas");
+            canvas = myGO.AddComponent<Canvas>();
+            canvas.renderMode = RenderMode.ScreenSpaceOverlay;
+            myGO.AddComponent<CanvasScaler>();
+            myGO.AddComponent<GraphicRaycaster>();
+            myGO.layer = 5;
+        }
+        var menuBuilder = new GameObject("Menu Builder");
+        menuBuilder.transform.parent = canvas.transform;
+        var rectTransform = menuBuilder.AddComponent<RectTransform>();
+        var menu = menuBuilder.AddComponent<Menu>();
+        var verticalLayoutGroup = menuBuilder.AddComponent<VerticalLayoutGroup>();
+        rectTransform.anchorMin = Vector2.zero;
+        rectTransform.anchorMax = Vector2.one;
+        rectTransform.sizeDelta = Vector2.zero;
+        rectTransform.anchoredPosition = Vector2.zero;
+        verticalLayoutGroup.childForceExpandHeight = false;
+        verticalLayoutGroup.childAlignment = TextAnchor.MiddleCenter;
+        var allAssetPaths = AssetDatabase.FindAssets("t:"+ nameof(MenuElementConfig));
+        var paths = allAssetPaths.Select(AssetDatabase.GUIDToAssetPath);
+        menu.menuElementConfigs = paths.Select(AssetDatabase.LoadAssetAtPath<MenuElementConfig>).ToArray();
+    }
+}
--- Assets/Scripts/MenuElementConfig.cs
+++ Assets/Scripts/MenuElementConfig.cs
@@ -9,7 +9,7 @@ public abstract class MenuElementConfig : ScriptableObject
     public GameObject objectToCreate;
     public string[] Arguments { get; private set; }
     public string[] Descriptions { get; private set; }
-    public bool canCombineArgs;
+    [HideInInspector] public bool canCombineArgs;
 
     public virtual void Awake()
     {
